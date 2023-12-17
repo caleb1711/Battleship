f2fe9293c9d4c259f4448736f2f2b5f1d7ffd5b0
caleb1711 <>
2023-12-17 19:54:11 +0000

    create a method to check if all the positions are valid

--- run.py
+++ run.py
@@ -5,6 +5,18 @@ import time
 
 
 
+"""
+    Legend:
+    1. "." = water or empty space
+    2. "O" = part of ship
+    3. "X" = part of ship that was hit with bullet
+    4. "#" = water that was shot with bullet, a miss because it hit no ship
+"""
+
+
+
+
+
 
 # Global variable for grid
 grid = [[]]
@@ -23,3 +35,39 @@ ship_positions = [[]]
 # Global variable for alphabet
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
+
+def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
+    """Will check the row or column to see if it is safe to place a ship there.
+       Return True or False.
+    """
+    global grid
+    global ship_positions
+
+
+    all_valid = True
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            if grid[r][c] != ".":
+                all_valid = False
+                break
+    if all_valid:
+        ship_positions.append([start_row, end_row, start_col, end_col])
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                grid[r][c] = "O"
+    return all_valid
+
+
+    
+
+
+def try_to_place_ship_on_grid(row, col, direction, length):
+    """Based on direction will call helper method to try and place a ship on the grid.
+       Returns validate_grid_and_place_ship which will be True or False.
+    """
+    global grid_size
+
+    pass
+
+    return validate_grid_and_place_ship(0, 0, 0, 0)
