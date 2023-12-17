20e907515def4f461f97400a3a271da91b842353
caleb1711 <>
2023-12-17 20:59:03 +0000

    create a method to handle the directions of the ship

--- run.py
+++ run.py
@@ -63,11 +63,28 @@ def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
 
 
 def try_to_place_ship_on_grid(row, col, direction, length):
-    """Based on direction will call helper method to try and place a ship on the grid.
-       Returns validate_grid_and_place_ship which will be True or False.
-    """
+    """Based on direction will call helper method to try and place a ship on the grid"""
     global grid_size
 
-    pass
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "left":
+        if col - length < 0:
+            return False
+        start_col = col - length + 1
 
-    return validate_grid_and_place_ship(0, 0, 0, 0)
+    elif direction == "right":
+        if col + length >= grid_size:
+            return False
+        end_col = col + length
+
+    elif direction == "up":
+        if row - length < 0:
+            return False
+        start_row = row - length + 1
+
+    elif direction == "down":
+        if row + length >= grid_size:
+            return False
+        end_row = row + length
+
+    return validate_grid_and_place_ship(start_row, end_row, start_col, end_col)
