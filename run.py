a88d63d66bdce2274b48df761f09777791c1ceac
caleb1711 <>
2023-12-17 19:15:31 +0000

    create variables for the game's grid, grid size, number of ships, bullets left, game over, number of ships sunk, ship_positions and alphabet

--- run.py
+++ run.py
@@ -1,2 +1,25 @@
 import random
 import time
+
+
+
+
+
+
+# Global variable for grid
+grid = [[]]
+# Global variable for grid size
+grid_size = 10
+# Global variable for number of ships to place
+num_of_ships = 8
+# Global variable for bullets left
+bullets_left = 50
+# Global variable for game over
+game_over = False
+# Global variable for number of ships sunk
+num_of_ships_sunk = 0
+# Global variable for ship positions
+ship_positions = [[]]
+# Global variable for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
